5806ff76335051951247d6c05be9acf55be9e422
Bernardo Hernandez <bhernandez@consistent.com.mx>
2019-08-12 22:25:38 -0500

    configutarion

--- src/main/java/com/consistent/interfaces/Configuration.java
+++ src/main/java/com/consistent/interfaces/Configuration.java
@@ -1,9 +1,14 @@
 package com.consistent.interfaces;
 
 import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+import aQute.bnd.annotation.metatype.Meta;
 
 @ExtendedObjectClassDefinition(category = "Posadas", scope  = ExtendedObjectClassDefinition.Scope.SYSTEM)
-@Meta
+@Meta.OCD (localization = "content/Language", id = "com.consistent.interfaces.Configuration", name = "Service RoomList")
 public interface Configuration {
-
+	
+	@Meta.AD(required = false, description = "Get Folder Parent Name", deflt="0")
+	public String getFolderParent();
+	
+	
 }
